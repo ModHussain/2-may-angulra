f4785d49ebcca51f73cd49eba04eede54cdf149b
Satish  Kumar Dawala <dsatishkumar@OJAS-IT.COM>
2018-05-02 16:16:49 +0530

    Signed-off-by: ModHussain

---
+++ js/directive.js
@@ -0,0 +1,11 @@
+resourceApp.directive('searchbar',function() {
+  	return {
+   	 		templateUrl:'partials/searchbar.html',
+			restrict:'EAC',
+			replace: true,
+			scope:{
+
+				}
+			
+  			};
+});
